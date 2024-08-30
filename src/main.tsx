6bfe499fe4d3211a319b88f767933f6f6df9c3e4
GCORP LLC <hosseinradvictor@gmail.com>
2024-08-30 09:56:47 +0330

    install react Query

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,19 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { QueryClientProvider } from '@tanstack/react-query'
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
+  
   <StrictMode>
  <QueryClientProvider client={queryClient}>
     <App />
