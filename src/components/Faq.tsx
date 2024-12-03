8e290c899b0b27adfdf257df455fb2813a7b7853
GCORP LLC <hosseinradvictor@gmail.com>
2024-12-03 20:28:25 +0330

    Set Faq And Product Page

---
+++ src/components/Faq.tsx
@@ -0,0 +1,156 @@
+export default function Faq() {
+    return (
+      <>
+        <section className="sm:py-10 bg-amber-500 overflow-hidden" id="faq">
+          <div className="container mx-auto px-4">
+            <div className="relative py-5 px-8 bg-yelloww overflow-hidden rounded-3xl">
+              <div className="relative z-10 md:max-w-7xl mx-auto">
+                <div className="md:max-w-xl mb-10">
+                  <span className="inline-block mb-5 text-md text-white font-bold uppercase tracking-widest">
+                    Frequently asked questions
+                  </span>
+                  <h2 className="font-heading text-4xl   text-white text-4xl lg:text-5xl font-bold font-heading">
+                    Got questions? We’re here to help!
+                  </h2>
+                </div>
+                <div className="flex flex-wrap -m-3">
+                  <div className="w-full p-3">
+                    <div className="p-5 bg-base-100 rounded-3xl">
+                      <div className="flex flex-wrap -m-2">
+                        <div className="w-full md:w-1/2 p-2">
+                          <h3 className="font-heading text-xl text-white font-black">
+                            How soon will I receive the voucher code once I make
+                            the payment?
+                          </h3>
+                        </div>
+                        <div className="w-full md:w-1/2 p-2">
+                          <p className="text-white font-medium">
+                            You will receive the voucher code immediately via
+                            email and SMS once you make the payment. The process
+                            is completely automated and takes only 30 seconds.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="w-full p-3">
+                    <div className="p-5 bg-base-100 rounded-3xl">
+                      <div className="flex flex-wrap -m-2">
+                        <div className="w-full md:w-1/2 p-2">
+                          <h3 className="font-heading text-xl text-white font-black">
+                            Within what timeframe do I need to book the slot
+                            after purchasing the voucher?
+                          </h3>
+                        </div>
+                        <div className="w-full md:w-1/2 p-2">
+                          <p className="text-white font-medium">
+                            This voucher is valid for 11 months. It is important
+                            to complete your exam within this 11-month period
+                            starting from the date of purchase.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="w-full p-3">
+                    <div className="p-5 bg-base-100 rounded-3xl">
+                      <div className="flex flex-wrap -m-2">
+                        <div className="w-full md:w-1/2 p-2">
+                          <h3 className="font-heading text-xl text-white font-black">
+                            Is this refundable?
+                          </h3>
+                        </div>
+                        <div className="w-full md:w-1/2 p-2">
+                          <p className="text-white font-medium">
+                            Sure, if you change your mind and plan not to appear
+                            for Toefl Academic and return the unused voucher
+                            code to us, we will happily refund 70% of the amount
+                            within 6 months of purchase.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="w-full p-3">
+                    <div className="p-5 bg-base-100 rounded-3xl">
+                      <div className="flex flex-wrap -m-2">
+                        <div className="w-full md:w-1/2 p-2">
+                          <h3 className="font-heading text-xl text-white font-black">
+                            Can I book any available slot?
+                          </h3>
+                        </div>
+                        <div className="w-full md:w-1/2 p-2">
+                          <p className="text-white font-medium">
+                            Yes, you can book any available slot across India.
+                            You just need to book your slot 48 hours in advance.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="w-full p-3">
+                    <div className="p-5 bg-base-100 rounded-3xl">
+                      <div className="flex flex-wrap -m-2">
+                        <div className="w-full md:w-1/2 p-2">
+                          <h3 className="font-heading text-xl text-white font-black lg:pr-10">
+                            Do I have to pay anything extra at PearsonToefl.com
+                            website at the time of slot booking?
+                          </h3>
+                        </div>
+                        <div className="w-full md:w-1/2 p-2">
+                          <p className="text-white font-medium">
+                            No, you don't have to pay anything extra on
+                            PearsonToefl.com website to book your slot. This
+                            prepaid Toefl voucher is inclusive of GST and covers
+                            the full cost of Toefl Academic.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="w-full p-3">
+                    <div className="p-5 bg-base-100 rounded-3xl">
+                      <div className="flex flex-wrap -m-2">
+                        <div className="w-full md:w-1/2 p-2">
+                          <h3 className="font-heading text-xl text-white font-black">
+                            Can you book my slot for me?
+                          </h3>
+                        </div>
+                        <div className="w-full md:w-1/2 p-2">
+                          <p className="text-white font-medium">
+                            Sure, if you're new to TOEFL and GRE and don't know
+                            how to book your slot, our team will happily help
+                            you to book your slot or we will book it for you.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="w-full p-3">
+                    <div className="p-5 bg-base-100 rounded-3xl">
+                      <div className="flex flex-wrap -m-2">
+                        <div className="w-full md:w-1/2 p-2">
+                          <h3 className="font-heading text-xl text-white font-black">
+                            Can you give me some more discount?
+                          </h3>
+                        </div>
+                        <div className="w-full md:w-1/2 p-2">
+                          <p className="text-white font-medium">
+                            Unfortunately, we cannot provide any further
+                            discount. There is no room for any additional
+                            discount since these are genuine vouchers and they
+                            include 18% GST, unlike others.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </>
+    );
+}
---
+++ src/components/HomePorduct.tsx
@@ -0,0 +1,76 @@
+import ProductCard from "./ProductCard";
+
+
+export default function HomeProduct() {
+  // لیست محصولات
+  const products = [
+    {
+      id: 1,
+      name: "محصول 1",
+      price: 20000,
+      discount: 10,
+      image: "/img/product/coffee.avif",
+    },
+    {
+      id: 2,
+      name: "محصول 2",
+      price: 50000,
+      discount: 5,
+      image: "/img/product/coffee-2.avif",
+    },
+    {
+      id: 3,
+      name: "محصول 3",
+      price: 75000,
+      discount: 15,
+      image: "/img/product/coffee-3.avif",
+    },
+    {
+      id: 4,
+      name: "محصول 4",
+      price: 100000,
+      discount: 20,
+      image: "/img/product/coffee-4.avif",
+    },
+    {
+      id: 5,
+      name: "محصول 5",
+      price: 30000,
+      discount: 0,
+      image: "/img/product/ice-cream-4.jpg",
+    },
+    {
+      id: 6,
+      name: "محصول 6",
+      price: 120000,
+      discount: 25,
+      image: "/img/product/ice-cream-3.jpg",
+    },
+    {
+      id: 7,
+      name: "محصول 7",
+      price: 45000,
+      discount: 10,
+      image: "/img/product/ice-cream-2.jpg",
+    },
+    {
+      id: 8,
+      name: "محصول 8",
+      price: 80000,
+      discount: 30,
+      image: "/img/product/ice-cream-1.jpg",
+    },
+   
+  ];
+  return (
+    <>
+      <div className="content-center m-3">
+        <div className="grid md:grid-cols-4 grid-cols-1 m-6 md:gap-2 justify-center">
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
