344b9d94f7d9038501fa6ed75a2faea0f96b9b51
GCORP LLC <hosseinradvictor@gmail.com>
2024-12-29 13:59:46 +0330

    Create FaqItem Components For static data

---
+++ src/components/FaqItem.tsx
@@ -0,0 +1,28 @@
+export default function FaqItem() {
+    return (
+      <>
+        <div className="w-full p-3">
+          <div className="p-5 bg-base-100 rounded-3xl">
+            <div className="flex flex-wrap -m-2">
+              <div className="w-full md:w-1/2 p-2">
+                <h3 className="font-heading text-xl text-white font-black">
+                  آیا قهوه چربی سوز است ؟
+                </h3>
+              </div>
+              <div className="w-full md:w-1/2 p-2">
+                <p className="text-white font-medium">
+                  تناسب اندام شاید یکی از مهمترین چیزایی که آدما این روزا بهش
+                  توجه میکنن. هر چی شما تناسب اندام بهتری داشته باشین اعتماد به
+                  نفس بهتر و البته سلامت بدنی بهتری دارین . از قدیم هم که گفتن
+                  عقل سالم در بدن سالم پس چی بهتر از یه بدن ورزیده میتونه باشه.
+                  خب دور نشیم از موضوع اصلی، بله قهوه به تناسب اندام کمک میکنه
+                  اما به یک شرط.اونم اینکه وقتی قهوه خوردید ورزش کنید و تحرک
+                  داشته باشین .
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+}
